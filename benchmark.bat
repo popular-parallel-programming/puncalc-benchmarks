7f4914fef01c730bc0e4e9896ebc3093a4821335
Florian Biermann <fbie@itu.dk>
2017-12-01 15:15:42 +0100

    Update benchmarking script and layout

--- benchmark.bat
+++ benchmark.bat
@@ -2,18 +2,15 @@
 if "%1" == "" goto :help
 if "%2" == "" goto :help
 
-:: Sequential
 call git checkout origin/parallel
-call :benchmark %1 %2 logs\seq -n -t
 
-:: Parallel without thread local optimization
-echo WARNING: origin/parallel-no-thread is disabled!
-::call git checkout origin/parallel-no-thread-local
-::call :benchmark %1 %2 logs\no-thread-local
+:: Sequential
+call :benchmark %1 %2 logs\seq -n
+call :benchmark %1 %2 logs\seq-local -n -l
 
 :: Parallel
-call git checkout origin/parallel
-call :benchmark %1 %2 logs\parallel -t
+call :benchmark %1 %2 logs\par
+call :benchmark %1 %2 logs\par-local -l
 
 exit /b
 
@@ -33,7 +30,8 @@ setlocal
 set files=%1
 set n=%2
 set log=%3
-set flags=%4
+shift /3
+set flags=%*
 mkdir %log%
 
 :: Log build events
@@ -45,7 +43,7 @@ call build -r %flags% >> %log%\build.log 2>&1
 :: Benchmark Funcalc for each sheet.
 for /r %files% %%I in (*.xml) do (
     echo Benchmarking %%I
-    call funcalc -r full %n% "%%I" 1> "%log%\%%~nxI.out" 2> "%log%\%%~nxI.err"
+    call funcalc -r roots %n% "%%I" 1> "%log%\%%~nxI.out" 2> "%log%\%%~nxI.err"
 )
 
 echo Done!
